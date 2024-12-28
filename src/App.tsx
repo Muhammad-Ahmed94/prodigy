672a7b8e3002a99eb733a1331073c5a26786345b
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2024-12-28 15:05:51 +0500

    Config: Tailwind config. Font styles and colors added.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold uppercase text-center'>
+    <div className='text-3xl font-bold text-center'>
       <h1 className='underline'>Prodigy</h1>
-      <p className='italic'>Award winnig website</p>
+      <p className='font-circular_web'>this is font ciruclar</p>
+      <p className='font-general'>this is font general</p>
+      <p className='font-robert_medium'>this is font robert medium</p>
+      <p className='font-robert_regular'>this is font robert rogeular</p>
+      <p className='font-zentry_regular'>this is font zentry regular</p>
+      <p className='text-blue-50'>this is font zentry regular</p>
+      <p className='text-blue-75'>this is font zentry regular</p>
+      <p className='text-blue-100'>this is font zentry regular</p>
+      <p className='text-blue-200'>this is font zentry regular</p>
+      <p className='text-blue-300'>this is font zentry regular</p>
+      <p className='text-voilet-300'>this is font zentry regular</p>
+      <p className='text-yellow-100'>this is font zentry regular</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        circular_web: ['circular-web', 'sans-serif'],
+        general: ['general', 'sans-serif'],
+        robert_medium: ['robert-medium', 'sans-serif'],
+        robert_regular: ['robert-regular', 'sans-serif'],
+        zentry_regular: ['zentry-regular', 'sans-serif']
+      },
+      colors: {
+        blue: {
+          50: '#dfdff0',
+          75: '#dfdff2',
+          100: '#f0f2fa',
+          200: '#010101',
+          300: '#4fb7dd',
+        },
+        voilet: {
+          300: '#5724ff'
+        },
+        yellow: {
+          100: '#8e983f'
+        }
+      }
+    }
   },
   plugins: [],
 };

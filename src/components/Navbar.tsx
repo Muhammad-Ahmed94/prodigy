cf02cf02ab1fc51d0cd8e720c7f0fc8d523daa57
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2025-01-04 14:58:28 +0500

    Feat: Navbar hides on scroll down and appear on scroll up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,16 @@
-import { useRef, useState } from 'react';
-import { GiClick } from 'react-icons/gi';
-import { MdMusicNote, MdMusicOff } from 'react-icons/md';
+import { useEffect, useRef, useState } from "react";
+import { GiClick } from "react-icons/gi";
+import { MdMusicNote, MdMusicOff } from "react-icons/md";
 
-import Button from './Button';
+import Button from "./Button";
 
 const Navbar = () => {
   const [playAudio, setPlayAudio] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+
   const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const lastScrollY = useRef(0);
   const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
 
   const toggleAudio = () => {
@@ -21,51 +25,70 @@ const Navbar = () => {
     }
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if(currentScrollY > lastScrollY.current) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+      lastScrollY.current = currentScrollY;
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+
+  }, [])
+
   return (
-    <div className="fixed inset-x-0 top-4 border-none rounded-full transition-all duration-700 h-9 z-50 sm:inset-x-6 bg-[#2C2C2C] text-white">
-      <header className=" ">
-        <nav className="flex justify-between items-center gap-4">
-          <div className="flex justify-center items-center gap-4">
-            <img
-              src="/img/logo.png"
-              alt="avatar_logo"
-              height={40}
-              width={40}
-              className="rounded-full object-contain"
-            />
-            <Button title="Click me" styles="bg-slate-500" icon={GiClick} />
-          </div>
-          <div className="flex justify-between items-center gap-4">
-            {navItems.map((item) => (
-              <a
-                key={item}
-                href={`#${item.toLowerCase()}`}
-                className="hover:bg-[#64748b] hover:text-white text-md hover:underline rounded-3xl transition-all ease-in-out duration-300 px-2 py-1"
-              >
-                {item}
-              </a>
-            ))}
-            <button
-              title="Play audio"
-              onClick={toggleAudio}
-              className="flex items-center"
+    <header
+      className={`inset-x-0 top-0 border-none rounded-full transition-all duration-500 h-12 fixed z-50 sm:inset-x-6 text-white ${
+        isVisible ? "translate-y-0 bg-[#2C2C2C]" : "-translate-y-full"
+      }`}
+    >
+      <nav className="flex justify-between items-center gap-4">
+        <div className="flex justify-center items-center gap-4">
+          <img
+            src="/img/logo.png"
+            alt="avatar_logo"
+            height={40}
+            width={40}
+            className="rounded-full object-contain"
+          />
+          <Button title="Click me" styles="bg-slate-500" icon={GiClick} />
+        </div>
+        <div className="flex justify-between items-center gap-4">
+          {navItems.map((item) => (
+            <a
+              key={item}
+              href={`#${item.toLowerCase()}`}
+              className="hover:bg-[#64748b] hover:text-white text-md hover:underline rounded-3xl transition-all ease-in-out duration-300 px-2 py-1"
             >
-              <audio
-                ref={audioRef}
-                src="/audio/loop.mp3"
-                loop
-                className="hidden"
-              />
-              {playAudio ? (
-                <MdMusicOff className="h-6 w-6" />
-              ) : (
-                <MdMusicNote className="h-6 w-6" />
-              )}
-            </button>
-          </div>
-        </nav>
-      </header>
-    </div>
+              {item}
+            </a>
+          ))}
+          <button
+            title="Play audio"
+            onClick={toggleAudio}
+            className="flex items-center"
+          >
+            <audio
+              ref={audioRef}
+              src="/audio/loop.mp3"
+              loop
+              className="hidden"
+            />
+            {playAudio ? (
+              <MdMusicOff className="h-6 w-6" />
+            ) : (
+              <MdMusicNote className="h-6 w-6" />
+            )}
+          </button>
+        </div>
+      </nav>
+    </header>
   );
 };
 
